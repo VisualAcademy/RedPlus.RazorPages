d6cc91856958ecc5436f3c6fee126405d15c3219
RedPlus <redplus@redplus.net>
2021-01-23 15:35:59 +0000

    Razor Page 사용 설정 후 Razor Page를 사용하여 사이트 버전 및 작성자 표시하는 페이지 생성

--- RedPlus/Startup.cs
+++ RedPlus/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +12,7 @@ namespace RedPlus
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages(); // 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +27,7 @@ namespace RedPlus
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages(); //
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!!!");
