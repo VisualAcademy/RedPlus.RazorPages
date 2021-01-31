3f67fa881e74a00b84be18d06df8c3452fdbf188
RedPlus <redplus@redplus.net>
2021-02-01 06:27:00 +0900

    Portfolio Data Modeling

---
+++ RedPlus/Models/Portfolio.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace RedPlus.Models
+{
+    /// <summary>
+    /// 모델 클래스: Model, ViewModel, Dto, Object, Entity, ...
+    /// </summary>
+    public class Portfolio
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Portfolio>(this);
+        }
+    }
+}
