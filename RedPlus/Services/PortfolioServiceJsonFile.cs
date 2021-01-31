15e306ee77517c8f03ec57824bd7d4224048532d
RedPlus <redplus@redplus.net>
2021-01-31 23:03:42 +0000

    JSON 파일을 읽어들여 컬렉션으로 변환해주는 서비스 클래스 만들고 사용하기

---
+++ RedPlus/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,22 @@
+﻿using RedPlus.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace RedPlus.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio> GetPortfolios()
+        {
+            var jsonFileName = @"C:\Razor\RedPlus.RazorPages\RedPlus\wwwroot\Portfolios\portfolios.json";
+
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+                return portfolios;
+            }
+        }
+    }
+}
